bf809737d950c02967446468885549254f57221e
derMacon <silash169@gmail.com>
2020-09-30 22:38:05 +0200

    updated dummy project

--- demos/spring_api/src/main/java/com/example/restservice/GreetingController.java
+++ demos/spring_api/src/main/java/com/example/restservice/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.restservice;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,10 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public List<Item> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	    List<Item> l = new LinkedList<>();
+	    l.add(new Item("test1", 42));
+		l.add(new Item("test2", 43));
+		return l;
 	}
 }
