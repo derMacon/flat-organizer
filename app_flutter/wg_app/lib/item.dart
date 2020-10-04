f746d1d550619bb40c168b1420bc3b04e974dd59
derMacon <silash169@gmail.com>
2020-10-04 18:30:26 +0200

    added post request

--- app_flutter/wg_app/lib/item.dart
+++ app_flutter/wg_app/lib/item.dart
@@ -2,12 +2,14 @@ class Item {
 
   String name;
   int count;
+  String requester;
 
-  Item({this.name, this.count});
+  Item({this.name, this.count, this.requester});
 
   Item.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     count = json['count'];
+    requester = json['requester'];
   }
 
 }
--- app_flutter/wg_app/lib/main.dart
+++ app_flutter/wg_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'item.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
@@ -16,18 +16,32 @@ class ItemList extends StatefulWidget {
 
 class _ItemListState extends State<ItemList> {
   List<Item> items = [
-    Item(name: 'item 1', count: 3),
-
-    // Item(name: 'item 1', count: 3, holder: 'Fritz'),
-    // Item(name: 'item 2', count: 4, holder: 'Bob'),
-    // Item(name: 'item 3', count: 6, holder: 'Gundula'),
+    Item(name: 'item 1', count: 3, requester: 'req1'),
   ];
+
+  /**
+   * Sends Post request to server using json syntax
+   */
+  Future<void> sendPost() async {
+    var url = "http://10.0.2.2:8080/post";
+    var body = json.encode({"name": "send name", "count": "8", "requester": "send requester"});
+    Map<String,String> headers = {
+      'Content-type' : 'application/json',
+      'Accept': 'application/json',
+    };
+
+    http.Response response = await http.post(url, body: body, headers: headers);
+    final responseJson = json.decode(response.body);
+    print(responseJson);
+    print("sendPost");
+  }
 
   @override
   void initState() {
     super.initState();
     print('initState function ran');
     updateList();
+    sendPost();
   }
 
   /**
@@ -45,7 +59,7 @@ class _ItemListState extends State<ItemList> {
    * Fetch items from endpoint
    */
   Future<List<Item>> getData() async {
-    Response response = await get("http://10.0.2.2:8080/greeting");
+    http.Response response = await http.get("http://10.0.2.2:8080/greeting");
     var parsed = jsonDecode(response.body);
     List<Item> item_lst =
         (parsed as List).map((data) => new Item.fromJson(data)).toList();
@@ -64,7 +78,7 @@ class _ItemListState extends State<ItemList> {
       ),
       body: Column(
         children:
-            items.map((item) => Text('${item.name} - ${item.count}')).toList(),
+            items.map((item) => Text('${item.name} - ${item.count} - ${item.requester}')).toList(),
       ),
     );
   }
