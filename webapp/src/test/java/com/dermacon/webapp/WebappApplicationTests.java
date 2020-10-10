e19f3907226fe93bd7d41cd0b9efa7e93d5f4680
derMacon <silash169@gmail.com>
2020-10-10 20:08:55 +0200

    initialized spring webapp

---
+++ webapp/src/test/java/com/dermacon/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dermacon.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
