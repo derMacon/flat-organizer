d7a3c15c8ba45f29d16ac3a675e1ad98ff0386d4
derMacon <silash169@gmail.com>
2020-10-11 18:59:10 +0200

    added first model access

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/DefaultController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/DefaultController.java
@@ -3,26 +3,34 @@ package com.dermacon.securewebapp.controller;
 import com.dermacon.securewebapp.data.User;
 import com.dermacon.securewebapp.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class DefaultController {
 
     @Autowired
     UserRepository userRepository;
 
     @RequestMapping("/")
-    public List<User> index() {
-        return (List<User>) userRepository.findAll();
-//        return "index";
+    public String index() {
+//        return (List<User>) userRepository.findAll();
+        return "index";
     }
 
     @RequestMapping("/noSecurity")
     public String noSecurity() {
         return "noSecurity";
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("msg", "Welcome to the Netherlands!");
     }
 
 }
