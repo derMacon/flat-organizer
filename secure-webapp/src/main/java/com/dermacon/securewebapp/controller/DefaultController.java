4a6fe96d781032c61ea69f7147aa269fe31a44c8
derMacon <silash169@gmail.com>
2020-10-11 23:26:47 +0200

    added model input to endpoint

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/DefaultController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/DefaultController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Controller
@@ -25,9 +28,18 @@ public class DefaultController {
     UserRepository userRepository;
 
     @RequestMapping("/")
-    public String index() {
+    public String index(@ModelAttribute("currUser") User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        user.setUsername(currentPrincipalName);
+
 //        return (List<User>) userRepository.findAll();
-        return "index";
+//        List<User> users = new LinkedList<>();
+//
+//        model.addAttribute("users", users);
+//        return "index";
+        return "noSecurity";
     }
 
     @RequestMapping("/noSecurity")
@@ -38,6 +50,8 @@ public class DefaultController {
     @ModelAttribute
     public void addAttributes(Model model) {
         List<User> users = (List<User>)userRepository.findAll();
+//        List<User> users = new LinkedList<>();
+
         model.addAttribute("users", users);
     }
 
