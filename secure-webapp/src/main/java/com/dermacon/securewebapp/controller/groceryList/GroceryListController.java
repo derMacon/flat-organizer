b90a14768902e7d47a7395b204eaf91667c01093
derMacon <silash169@gmail.com>
2020-10-15 03:05:48 +0200

    added checkboxes to articles

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/groceryList/GroceryListController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/groceryList/GroceryListController.java
@@ -1,4 +1,4 @@
-package com.dermacon.securewebapp.controller;
+package com.dermacon.securewebapp.controller.groceryList;
 
 import com.dermacon.securewebapp.data.Flatmate;
 import com.dermacon.securewebapp.data.FlatmateRepository;
@@ -13,6 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class GroceryListController {
@@ -29,17 +34,34 @@ public class GroceryListController {
 
     @GetMapping("/groceryList")
     public String showForm(Model model) {
+        // adding item which will be set in the thymeleaf form and used
+        // and overwritten when a new item will be added
         Item item = new Item();
         model.addAttribute("item", item);
 
+        // add items to the model which will be displayed by thymeleaf
         Iterable<Item> saved_items = itemRepository.findAll();
         model.addAttribute("saved_items", saved_items);
 
-//        for (Item currItem : saved_items) {
-//            System.out.print(currItem + ", ");
-//        }
+        List<SelectableItem> selectableItems = new LinkedList<>();
+        for (Item currItem : saved_items) {
+            selectableItems.add(new SelectableItem(currItem, false));
+
+        }
+
+        model.addAttribute("selectable_items", selectableItems);
 
         return "groceryList";
+    }
+
+    @PostMapping("/groceryList/removeFromList")
+    public String removeItems(@ModelAttribute("selectable_items") Iterable<Item> items_selected) {
+
+        for (Item currItem : items_selected) {
+            System.out.print(currItem + ", ");
+        }
+
+        return "redirect:/groceryList";
     }
 
 
