e0e9543268dca7d3c328a1c2397b6e308a183e55
derMacon <silash169@gmail.com>
2020-11-06 03:52:07 +0100

    updated header

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/DefaultController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/DefaultController.java
@@ -3,12 +3,17 @@ package com.dermacon.securewebapp.controller;
 import com.dermacon.securewebapp.data.User;
 import com.dermacon.securewebapp.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -20,7 +25,6 @@ public class DefaultController {
 
     @RequestMapping("/")
     public String index(Model model) {
-//        model.addAttribute("selectedDomain", "home");
         return "redirect:/groceryList";
     }
 
@@ -29,6 +33,18 @@ public class DefaultController {
         List<User> users = (List<User>) userRepository.findAll();
 
         model.addAttribute("users", users);
+    }
+
+    /**
+     * https://stackoverflow.com/questions/20848312/how-to-correctly-logout-user-in-spring-security
+     */
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout"; //You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 
 
