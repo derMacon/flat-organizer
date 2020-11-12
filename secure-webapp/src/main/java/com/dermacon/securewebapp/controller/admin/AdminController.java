03a0b4d1e0e4d4287a4f6066a68970b7093561b3
derMacon <silash169@gmail.com>
2020-11-12 12:49:41 +0100

    updated gui

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/admin/AdminController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/admin/AdminController.java
@@ -11,6 +11,7 @@ import com.dermacon.securewebapp.data.UserRepository;
 import com.dermacon.securewebapp.data.UserRole;
 import com.dermacon.securewebapp.logger.LoggerSingleton;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -44,6 +45,9 @@ public class AdminController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @RequestMapping(value = "/groceryList/admin", method = RequestMethod.GET)
@@ -113,7 +117,8 @@ public class AdminController {
         User newUser = new User();
 
         newUser.setUsername(generateUsername(flatmate));
-        newUser.setPassword(generatePassword(flatmate));
+        String hash = passwordEncoder.encode(generatePassword(flatmate));
+        newUser.setPassword(hash);
         newUser.setRole(UserRole.ROLE_USER);
 
         return newUser;
