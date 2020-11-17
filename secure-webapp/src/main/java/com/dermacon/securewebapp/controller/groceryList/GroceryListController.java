1677d8416951e39aab7200975cf9cc64393bee98
derMacon <silash169@gmail.com>
2020-11-17 22:06:30 +0100

    updated password generator

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/groceryList/GroceryListController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/groceryList/GroceryListController.java
@@ -101,8 +101,8 @@ public class GroceryListController {
         model.addAttribute("available_categories", categories);
         model.addAttribute("new_preset", new ItemPreset());
 
-//        return "groceryList";
-        return "construction";
+        return "groceryList";
+//        return "construction";
     }
 
     @RequestMapping(value = "/processForm", method=RequestMethod.POST, params = "updateAll")
