cfad5594c8a360667d0d461e31febc6c41aeba85
derMacon <silash169@gmail.com>
2020-12-11 23:19:57 +0100

    added second http endpoint with basic auth for api requests

--- secure-webapp/src/main/java/com/dermacon/securewebapp/security/conf/WebSecurityConfig.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/security/conf/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,90 +12,106 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
-import java.time.Year;
 import java.util.concurrent.TimeUnit;
 
-import static com.dermacon.securewebapp.data.UserRole.ROLE_USER;
-
-@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
 
-    @Autowired
-    @Qualifier("dataSource")
-    private DataSource dataSource;
-
-    /**
-     * source: http://cristianruizblog.com/spring-security-persistent-token-2/
-     * @return
-     */
-    @Bean
-    public PersistentTokenRepository persistentTokenRepository() {
-        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
-        tokenRepository.setDataSource(dataSource);
-        return tokenRepository;
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/test")
+                    .authorizeRequests()
+                    .anyRequest().hasRole("ADMIN")
+                    .and()
+                    .httpBasic();
+        }
     }
 
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/noSecurity", "/css/*").permitAll()
-                .antMatchers("/test").hasRole("ADMIN")
-                .antMatchers("/groceryList/admin").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .rememberMe()
-                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(100))
+    @Configuration
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+
+        @Autowired
+        private UserDetailsService userDetailsService;
+
+        @Autowired
+        @Qualifier("dataSource")
+        private DataSource dataSource;
+
+        /**
+         * source: http://cristianruizblog.com/spring-security-persistent-token-2/
+         *
+         * @return
+         */
+        @Bean
+        public PersistentTokenRepository persistentTokenRepository() {
+            JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+            tokenRepository.setDataSource(dataSource);
+            return tokenRepository;
+        }
+
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/noSecurity", "/css/*").permitAll()
+                    .antMatchers("/test").hasRole("ADMIN")
+                    .antMatchers("/groceryList/admin").hasRole("ADMIN")
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .formLogin()
+                    .permitAll()
+                    .and()
+                    .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(100))
 //                    .tokenValiditySeconds((int)60)
                     .tokenRepository(persistentTokenRepository())
                     .userDetailsService(userDetailsService)
                     // random key, todo put into application.properties
                     .key("45lk432j5;lj435;l432j");
-    }
+        }
 
-    @Autowired
-    public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+        @Autowired
+        public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService);
+        }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return super.userDetailsService();
-    }
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return super.userDetailsService();
+        }
 
 
-    // ---------- hashing passwords -----------
+        // ---------- hashing passwords -----------
 
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public static PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
+        @Bean
+        public DaoAuthenticationProvider authProvider() {
+            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(userDetailsService);
+            authProvider.setPasswordEncoder(passwordEncoder());
+            return authProvider;
+        }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(authProvider());
+        }
     }
 }
