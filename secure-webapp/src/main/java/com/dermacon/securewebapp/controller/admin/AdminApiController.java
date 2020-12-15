4817f493d0c33d70a4b2fea3ba31deb4e734eaea
derMacon <silash169@gmail.com>
2020-12-15 16:34:50 +0100

    minor updates

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/admin/AdminApiController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/admin/AdminApiController.java
@@ -4,16 +4,12 @@ import com.dermacon.securewebapp.controller.services.FlatmateService;
 import com.dermacon.securewebapp.data.Flatmate;
 import com.dermacon.securewebapp.data.InputPerson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/admin")
