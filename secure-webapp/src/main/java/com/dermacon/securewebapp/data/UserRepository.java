c865d3d6d70dc31ae4be0677bcc9071c2aea0185
derMacon <silash169@gmail.com>
2021-01-26 20:26:06 +0100

    removed old project, trying to port to docker

--- secure-webapp/src/main/java/com/dermacon/securewebapp/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dermacon.securewebapp.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-    void deleteByUserId(long id);
-}
