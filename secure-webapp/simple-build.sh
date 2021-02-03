b4c402a23ca464861d4d48ff9e9bd4bda65a1888
derMacon <silash169@gmail.com>
2021-02-04 00:57:16 +0100

    don't know what this is exactly

--- secure-webapp/simple-build.sh
+++ secure-webapp/simple-build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo create external docker volume
-docker volume create --name=db-data
-
 echo build spring project
 mvn clean package
 
+echo start docker compose
 docker-compose -f docker-compose_production.yml down
 docker-compose -f docker-compose_production.yml up --build -d
